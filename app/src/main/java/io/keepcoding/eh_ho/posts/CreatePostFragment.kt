1fbab1fb0f4a51cd1052bbeab05b074188726b88
David Rabassa <gestionarlaweb@gmail.com>
2020-08-25 17:05:05 +0200

    Layout new Post creado

--- app/src/main/java/io/keepcoding/eh_ho/posts/CreatePostFragment.kt
+++ app/src/main/java/io/keepcoding/eh_ho/posts/CreatePostFragment.kt
@@ -1,14 +1,19 @@
 package io.keepcoding.eh_ho.posts
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import androidx.fragment.app.Fragment
 import io.keepcoding.eh_ho.R
 import io.keepcoding.eh_ho.inflate
+import kotlinx.android.synthetic.main.fragment_create_post.*
 
 class CreatePostFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -16,4 +21,36 @@ class CreatePostFragment : Fragment() {
     ): View? {
         return container?.inflate(R.layout.fragment_create_post)
     }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.menu_create_post, menu)
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Si se pulsa el boton enviar Post
+        //if(item.itemId == R.id.action_send_post)
+        //o  temabién así
+        when (item.itemId){
+            R.id.action_send_post -> createPost()
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    fun createPost() {
+        // Validar el campo inputPost del formulario
+        if (isFormValid()){
+            // Si es valido crear el Post
+        }else {
+            // Error
+            showErrors()
+        }
+    }
+
+    private fun showErrors() {
+        if (inputPost.text.isEmpty())
+            inputPost.error = getString(R.string.error_empty)
+    }
+
+    private fun isFormValid() = inputPost.text.isNotEmpty()
 }
